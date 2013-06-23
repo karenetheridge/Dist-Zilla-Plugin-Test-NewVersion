4f3dbbbf1f81add6e27b47672f759d0cf7a09652
Karen Etheridge <ether@cpan.org>
2013-06-23 12:17:18 -0700

    move pod up above DATA section, so podweaver inserts sections in the right order

--- lib/Dist/Zilla/Plugin/Test/NewVersion.pm
+++ lib/Dist/Zilla/Plugin/Test/NewVersion.pm
@@ -51,57 +51,6 @@ sub gather_files
     return;
 }
 
-__DATA__
-___[ xt/release/new-version.t ]___
-use strict;
-use warnings FATAL => 'all';
-
-use Encode;
-use LWP::UserAgent;
-use JSON;
-use Module::Runtime 'use_module';
-
-# returns bool, detailed message
-sub version_is_bumped
-{
-    my $pkg = shift;
-
-    my $ua = LWP::UserAgent->new(keep_alive => 1);
-    $ua->env_proxy;
-
-    my $res = $ua->get("http://cpanidx.org/cpanidx/json/mod/$pkg");
-    unless ($res->is_success) {
-        return (1, $pkg . ' not found in index - first release, perhaps?');
-    }
-
-    # JSON wants UTF-8 bytestreams, so we need to re-encode no matter what
-    # encoding we got. -- rjbs, 2011-08-18 (in Dist::Zilla)
-    my $json_octets = Encode::encode_utf8($res->decoded_content);
-    my $payload = JSON::->new->decode($json_octets);
-
-    unless (\@$payload) {
-        return (0, 'no valid JSON returned');
-    }
-
-    my $current_version = use_module($pkg)->VERSION;
-    return (0, $pkg . ' version is not set') if not defined $current_version;
-
-    my $indexed_version = version->parse($payload->[0]{mod_vers});
-    return (1) if $indexed_version < $current_version;
-
-    return (0, $pkg . ' is indexed at: ' . $indexed_version . '; local version is ' . $current_version);
-}
-
-foreach my $pkg (
-{{ join(",\n", map { '    q{' . $_ . '}' } @packages) }}
-)
-{
-    my ($bumped, $message) = version_is_bumped($pkg);
-    ok($bumped, $pkg . ' version is greater than version in index');
-    note $message if $message;
-}
-__END__
-
 =pod
 
 =head1 SYNOPSIS
@@ -153,3 +102,54 @@ Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Di
 I am also usually active on irc, as 'ether' at C<irc.perl.org>.
 
 =cut
+
+__DATA__
+___[ xt/release/new-version.t ]___
+use strict;
+use warnings FATAL => 'all';
+
+use Encode;
+use LWP::UserAgent;
+use JSON;
+use Module::Runtime 'use_module';
+
+# returns bool, detailed message
+sub version_is_bumped
+{
+    my $pkg = shift;
+
+    my $ua = LWP::UserAgent->new(keep_alive => 1);
+    $ua->env_proxy;
+
+    my $res = $ua->get("http://cpanidx.org/cpanidx/json/mod/$pkg");
+    unless ($res->is_success) {
+        return (1, $pkg . ' not found in index - first release, perhaps?');
+    }
+
+    # JSON wants UTF-8 bytestreams, so we need to re-encode no matter what
+    # encoding we got. -- rjbs, 2011-08-18 (in Dist::Zilla)
+    my $json_octets = Encode::encode_utf8($res->decoded_content);
+    my $payload = JSON::->new->decode($json_octets);
+
+    unless (\@$payload) {
+        return (0, 'no valid JSON returned');
+    }
+
+    my $current_version = use_module($pkg)->VERSION;
+    return (0, $pkg . ' version is not set') if not defined $current_version;
+
+    my $indexed_version = version->parse($payload->[0]{mod_vers});
+    return (1) if $indexed_version < $current_version;
+
+    return (0, $pkg . ' is indexed at: ' . $indexed_version . '; local version is ' . $current_version);
+}
+
+foreach my $pkg (
+{{ join(",\n", map { '    q{' . $_ . '}' } @packages) }}
+)
+{
+    my ($bumped, $message) = version_is_bumped($pkg);
+    ok($bumped, $pkg . ' version is greater than version in index');
+    note $message if $message;
+}
+__END__
